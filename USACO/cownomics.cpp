8ed607d265e561eccfd7aa22d34682863acbee06
Birb12 <53247327+Birb12@users.noreply.github.com>
2022-11-28 19:52:04 -0800

    Update cownomics.cpp

--- USACO/cownomics.cpp
+++ USACO/cownomics.cpp
@@ -1,64 +1,39 @@
 #include <vector>
 #include <iostream>
-#include <string>
 #include <fstream>
-#include <algorithm>
 #include <cmath>
-
-/*
-ID: Ian Atroshchenko [birb]
-TASK: cownomics
-LANG: C++                 
-*/
+#include <string>
+#include <algorithm>
+#include <unordered_map>
+#include <numeric>
 
 int main()
 {
-    std::ifstream fin ("cownomics.in");
-    std::ofstream fout ("cownomics.out");
-    int n, m, counter, num_differences;
-    num_differences = 0;
-    counter = 0;
-    fin >> n; fin >> m;
-    std::vector<std::string> cows, concat;
-    std::string concate, curr_string;
+    std::ifstream fin("cownomics.in");
+    std::ofstream fout("cownomics.out");
+    int n; fin >> n; int m; fin >> m; int positions = 0;
+    std::string spotty[n], plain[n];
 
-    for (int i = 0; i < n * 2; i++){
-        std::string curr_cow;
-        fin >> curr_cow;
-        cows.push_back(curr_cow);
+    for (std::string &s : spotty) {
+        fin >> s;
     }
 
-    for (int h = 0; h < m; h++){
-        for (int i = 0; i < n * 2; i++){        
-            curr_string.push_back(cows[i][h]);
-        }
-        concat.push_back(curr_string);
-        curr_string.clear();
+    for (std::string &s : plain) {
+        fin >> s;
     }
 
-    for (auto i : concat){
-        std::string substr1;
-        std::string substr2;
-        for (int j = 0; j < m; j++){
-            if (j >= n) {
-                substr2.push_back(i[j]);
-            } else {
-                substr1.push_back(i[j]);
+    for (int i = 0; i < m; i++) {
+        bool different = true;
+        for (int j = 0; j < n; j++) {
+            for (int k = 0; k < n; k++) {
+                if (spotty[j][i] == plain[k][i]){
+                    different = false;
+                }
             }
         }
 
-        for (int j = 0; j < n; j++){
-            char memory = substr1[j];
-
-            if (substr2.find(memory) == std::string::npos){
-                num_differences += 1;
-            } 
-        }
-
-        if (num_differences >= n){
-            num_differences = 0;
-            counter += 1;
-        }
+        positions += different;
     }
-    std::cout << counter << std::endl;
+
+    fout << positions;
 }
