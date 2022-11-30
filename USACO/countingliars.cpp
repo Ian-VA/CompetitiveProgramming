4113dbb90bb5dbbd6a13794c871f927891bc37a0
Birb12 <53247327+Birb12@users.noreply.github.com>
2022-11-30 12:16:09 -0800

    Update countingliars.cpp

--- USACO/countingliars.cpp
+++ USACO/countingliars.cpp
@@ -5,7 +5,7 @@
 int main() {
     int n; std::cin >> n;
     std::pair<char, int> cows[n];
-    int liars;
+    int liars = 0;
     
     for (int i = 0; i < n; i++) {
         std::pair<char, int> cow;
@@ -16,10 +16,31 @@ int main() {
     
     for (int i = 0; i < n; i++) {
         for (int j = i+1; j < n; j++) {
-            curr_cow = cows[i]; iterable_cow = cows[j];
+            std::pair<char, int>curr_cow = cows[i]; std::pair<char, int>iterable_cow = cows[j];
             
-
+            switch(curr_cow.first)
+            {
+                case 'G':
+                    switch (iterable_cow.first)
+                    {
+                        case 'L': 
+                            if ((iterable_cow.second - curr_cow.second) < 0){
+                                liars =+ 1;
+                            }
+                            break;
+                    }
+                case 'L':
+                    switch(iterable_cow.first)
+                    {
+                        case 'G':
+                            if ((curr_cow.second - iterable_cow.second) < 0){
+                                liars += 1;
+                            }
+                            break;
+                    }
+            }
         }
     }
     
-    return 0;
+    std::cout << liars;
+}
