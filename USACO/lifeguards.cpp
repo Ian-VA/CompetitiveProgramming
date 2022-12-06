fa51fcc4add7b95122de2d076706090614afaf03
Birb12 <53247327+Birb12@users.noreply.github.com>
2022-12-06 15:38:41 -0800

    Update lifeguards.cpp

--- USACO/lifeguards.cpp
+++ USACO/lifeguards.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <string>
 
 int main() {
   std::ofstream fout("lifeguards.out");
@@ -18,14 +19,39 @@ int main() {
 
   for (int i = 0; i < n; i++) {
     std::pair<int, int> firedcow = lifeguards[i];
-    std::cout << "FIREDCOW: " << lifeguards[i].first << " " << lifeguards[i].second << std::endl;
     for (int j = 0; j < n; j++) {
       if (lifeguards[j] != firedcow) {
-        int sum = 0;
         std::pair<int, int> curr_cow = lifeguards[j];
-        
-        max_time = std::max(max_time, sum);
+        for (int k = 0; k < n; k++) {
+          if (lifeguards[k] != firedcow && lifeguards[k] != lifeguards[j]) {
+            std::pair<int,int> compare_cow = lifeguards[k];
+            std::vector<int> rangecurr, rangecompare;
+
+            for (int l = curr_cow.first + 1; l <= curr_cow.second; l++) { // startpoints dont count as time
+              rangecurr.push_back(l);
+            }
+
+            for (int l = compare_cow.first + 1; l <= compare_cow.second; l++) {
+              rangecompare.push_back(l);
+            }
+            std::vector<int> delta;
+            for (auto &l : rangecurr) {
+              for (auto &m : rangecompare){
+                if (l != m) {
+                  delta.push_back(l);
+                }
+              }
+            }
+            int h = delta.size();
+            rangecurr.clear();
+            rangecompare.clear();
+            max_time = std::max(max_time, h);
+            delta.clear();
+          }
+        }
       }
     }
   }
+
+  std::cout << max_time << "\n";
 }
