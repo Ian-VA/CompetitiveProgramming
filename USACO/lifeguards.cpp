d3c1369f4c0e2f84e54d64847d1c1de263a73886
Birb12 <53247327+Birb12@users.noreply.github.com>
2022-12-09 09:27:21 -0800

    Update lifeguards.cpp

--- USACO/lifeguards.cpp
+++ USACO/lifeguards.cpp
@@ -2,56 +2,41 @@
 #include <fstream>
 #include <vector>
 #include <string>
+#include <algorithm>
 
 int main() {
   std::ofstream fout("lifeguards.out");
   std::ifstream fin("lifeguards.in");
-  int n; std::cin >> n;
+  int n; fin >> n;
   std::vector<std::pair<int, int>> lifeguards;
   int max_time = 0;
+  std::vector<int> allranges;
 
   for (int i = 0; i < n; i++) {
-    int start; int end; std::cin >> start; std::cin >> end;
+    int start; int end; fin >> start; fin >> end;
     std::pair<int, int> lifeguard; lifeguard.first = start; lifeguard.second = end;
 
     lifeguards.push_back(lifeguard);
   }
 
-  for (int i = 0; i < n; i++) {
-    std::pair<int, int> firedcow = lifeguards[i];
-    for (int j = 0; j < n; j++) {
-      if (lifeguards[j] != firedcow) {
-        std::pair<int, int> curr_cow = lifeguards[j];
-        for (int k = 0; k < n; k++) {
-          if (lifeguards[k] != firedcow && lifeguards[k] != lifeguards[j]) {
-            std::pair<int,int> compare_cow = lifeguards[k];
-            std::vector<int> rangecurr, rangecompare;
+  for (int k = 0; k < n; k++) {
+    std::pair<int, int> fired_lifeguard = lifeguards[k];
 
-            for (int l = curr_cow.first + 1; l <= curr_cow.second; l++) { // startpoints dont count as time
-              rangecurr.push_back(l);
-            }
+    for (auto i : lifeguards) {
+      if (i != fired_lifeguard) {
+        for (int l = i.first + 1; l <= i.second; l++) {
+          allranges.push_back(l);
+        } 
+    
+        std::sort(allranges.begin(), allranges.end());
+        allranges.erase(std::unique(allranges.begin(), allranges.end() ), allranges.end() );
 
-            for (int l = compare_cow.first + 1; l <= compare_cow.second; l++) {
-              rangecompare.push_back(l);
-            }
-            std::vector<int> delta;
-            for (auto &l : rangecurr) {
-              for (auto &m : rangecompare){
-                if (l != m) {
-                  delta.push_back(l);
-                }
-              }
-            }
-            int h = delta.size();
-            rangecurr.clear();
-            rangecompare.clear();
-            max_time = std::max(max_time, h);
-            delta.clear();
-          }
-        }
+        max_time = std::max(max_time, (int) allranges.size());
+
       }
     }
-  }
 
-  std::cout << max_time << "\n";
+    allranges.clear();
+  }   
+  fout << max_time << "\n";
 }
