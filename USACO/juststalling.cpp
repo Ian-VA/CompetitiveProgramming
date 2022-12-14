d420f7c51c99e80130b349f997982d56cd009100
Birb12 <53247327+Birb12@users.noreply.github.com>
2022-12-14 13:34:50 -0800

    Create juststalling.cpp

---
+++ USACO/juststalling.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+
+int main() {
+  int n; std::cin >> n;
+  int answer = 0;
+  std::vector<long long> cows, limits;
+
+  for (int i = 0; i < n; i++) {
+    int f; std::cin >> f; cows.push_back(f);
+  }
+
+  for (int i = 0; i < n; i++) {
+    int h; std::cin >> h; limits.push_back(h);
+  }
+
+  do
+  {
+    int occurences = 0;
+    for (int i = 0; i < n; i++) {
+      if (cows[i] <= limits[i]) {
+        occurences++;
+      }
+    }
+
+    if (occurences >= n) {
+      answer++;
+    }
+  } while (next_permutation(limits.begin(), limits.end()));
+
+  std::cout << answer;
+}
