f9dd3e2722e1d3edba49e92359e0a24324e518eb
Birb12 <53247327+Birb12@users.noreply.github.com>
2023-01-14 19:04:38 -0800

    Create littlelephant.cpp

---
+++ codeforces/littlelephant.cpp
@@ -0,0 +1,23 @@
+#include <vector>
+#include <iostream>
+#include <string>
+
+
+typedef long long ll;
+
+int main()
+{
+    std::string digits; std::cin >> digits;
+    int recorded_size = digits.size();
+
+    for (int i = 0; i < digits.size(); i++) {
+        if (digits[i] == '0'){
+            digits.erase(i, 1);
+            break;
+        }
+    }
+
+    if (digits.size() == recorded_size) {digits.erase(0, 1);}
+
+    std::cout << digits;
+}
