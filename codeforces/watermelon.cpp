90ab577eabd1fdb9fd9f8296e89043ee19793cd4
Birb12 <53247327+Birb12@users.noreply.github.com>
2023-01-14 18:53:03 -0800

    Create watermelon.cpp

---
+++ codeforces/watermelon.cpp
@@ -0,0 +1,13 @@
+#include <vector>
+#include <iostream>
+#include <string>
+
+
+typedef long long ll;
+
+int main()
+{
+    ll n, sum = 0; std::cin >> n;
+
+    if (n % 2 == 0 && n != 2) {std::cout << "YES";} else {std::cout << "NO";}
+}
