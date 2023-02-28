58fca324d2cbd525b7459a2f6a2fb488de18374b
Ian-VA <53247327+Ian-VA@users.noreply.github.com>
2023-02-27 18:10:31 -0800

    Update mootube.cpp

--- USACO/silver/mootube.cpp
+++ USACO/silver/mootube.cpp
@@ -1,41 +1,53 @@
 #include <iostream>
 #include <algorithm>
 #include <numeric>
+#include <utility>
 #include <vector>
+#include <fstream>
 
 typedef long long ll;
+#define max_n 5000
 
-std::vector<std::vector<std::pair<int, int>>> adj_list;
+std::vector<std::vector<std::pair<int, int>>> adj_list(max_n);
+int count = 0;
+std::vector<bool> visited;
 
-void dfs(int v, int k, std::vector<bool> visited, int count) {
-
-    if (visited[v]) 
+void dfs(int v, int k) {
+    if (visited[v])
         return;
 
     visited[v] = true;
 
     for (auto &n : adj_list[v]) {
-        if (n.second >= k) {count++;}
-        dfs(n.first, k, visited, count);
+        if (!visited[n.first] && k <= n.second) {
+            count++;
+            dfs(n.first, k);
+        }
     }
 }
 
 int main()
 {
-    int n, q; std::cin >> n >> q;
+    std::ofstream fout("mootube.out");
+    std::ifstream fin("mootube.in");
+
+    int n, q; fin >> n >> q;
     
     for (int i = 0; i < n-1; i++) {
-        int a, b, w; std::cin >> a >> b >> w;
+        int a, b, w; fin >> a >> b >> w;
+        a--; b--; 
 
-        adj_list[--a].push_back(std::make_pair(--b, w));
-        adj_list[--b].push_back(std::make_pair(--a, w));
+        adj_list[a].push_back({b, w});
+        adj_list[b].push_back({a, w});
     }
 
     for (int i = 0; i < q; i++) {
-        std::vector<bool> visited(n);
-        int k, v; std::cin >> k >> v;
-        int count = 0;
+        int k, v; fin >> k >> v;
+        count = 0;
+        v--;
+        visited = std::vector<bool>(n);
 
-        dfs(--v, k, visited, count);
+        dfs(v, k);
+        fout << count << "\n";
     }
 }
