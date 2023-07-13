dd99d8d6897fe5764abcdd3da4c39ef6c3088775
Ian-VA <53247327+Ian-VA@users.noreply.github.com>
2023-07-12 22:14:56 -0700

    Update waterstation.cpp

--- atcoder/waterstation.cpp
+++ atcoder/waterstation.cpp
@@ -9,7 +9,6 @@ int main() {
     
     if (n % 5 == 0) {std::cout << n;} else {
         int down = n, up = n;
-        
         while (down % 5 != 0 && up % 5 != 0) {
             if (--down % 5 == 0) {std::cout << down; break;}
             else if (++up % 5 == 0) {std::cout << up; break;}
