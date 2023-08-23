133e51e435a00a67708f9e115c0fb341f95adece
Ian-VA <53247327+Ian-VA@users.noreply.github.com>
2023-08-23 06:16:46 +0000

    Update binaryexponentiation.cpp

--- algorithms/binaryexponentiation.cpp
+++ algorithms/binaryexponentiation.cpp
@@ -14,10 +14,10 @@ using point = std::pair<i32, i32>;
 ll binpow(ll a, ll b) {
     ll res = 1;
     while (b > 0) {
-        if (b & 1)
+        if (b & 1) // if last bit is 1 (odd number)
             res = res * a;
-        a = a * a;
-        b >>= 1;
+        a = a * a; // if last bit is even, we can square again
+        b >>= 1; // right shift
     }
 
     return res;
